b3124d5bb0c374edf9c02ea6f7b6b037537e4738
Dietrich Epp <depp@zdome.net>
2015-02-23 05:30:03 -0800

    Make canvas fill the window

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@
 var gl = null;
 
 function start() {
-	var canvas = document.getElementById("game");
+	var canvas = document.getElementById("canvas");
 	try {
 		gl = canvas.getContext("webgl") ||
 			canvas.getContext("experimental-webgl");
@@ -13,9 +13,13 @@ function start() {
 		alert("Could not initialize WebGL.");
 		return;
 	}
+	canvas.width = canvas.clientWidth;
+	canvas.height = canvas.clientHeight;
 	gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
 	gl.clearColor(0.2, 0.5, 0.3, 0.0);
 	gl.clear(gl.COLOR_BUFFER_BIT);
+	var aspect = canvas.clientWidth / canvas.clientHeight;
+	console.log("Aspect: " + aspect);
 }
 
 start();
