7b86e29d6e4c048c8b40107cb19dc7cbc69d3ca7
Dietrich Epp <depp@zdome.net>
2015-02-23 05:35:41 -0800

    Add main animation loop

--- src/app.js
+++ src/app.js
@@ -15,11 +15,16 @@ function start() {
 	}
 	canvas.width = canvas.clientWidth;
 	canvas.height = canvas.clientHeight;
-	gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
-	gl.clearColor(0.2, 0.5, 0.3, 0.0);
-	gl.clear(gl.COLOR_BUFFER_BIT);
 	var aspect = canvas.clientWidth / canvas.clientHeight;
 	console.log("Aspect: " + aspect);
+	window.requestAnimationFrame(render);
+}
+
+function render(time) {
+	window.requestAnimationFrame(render);
+	gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
+	gl.clearColor(0.2, Math.sin(time*0.003)*0.5+0.5, 0.3, 0.0);
+	gl.clear(gl.COLOR_BUFFER_BIT);
 }
 
 start();
